5aa697c6a8933ba6bccb46a6fe5e6d7abf37ae21
TebogoLesedi1 <leseditebogo22@gmail.com>
2026-05-05 01:14:51 +0200

    feat: implement fact_credit table using float for financial metrics

---
+++ sql/fact_credit.sql
@@ -0,0 +1,26 @@
+--fact_table
+DROP TABLE IF EXISTS [credit_std].[dbo].[fact_credit];
+CREATE TABLE [credit_std].[dbo].[fact_credit](
+[loan_ID] INT IDENTITY(1,1) PRIMARY KEY,
+[Transaction_ID] [nvarchar](50) NOT NULL,
+[Annual_Income_ZAR] FLOAT NOT NULL,
+[Monthly_Expenses_ZAR] FLOAT NOT NULL,
+[Credit_Score] INT NOT NULL,
+[Loan_Amount_ZAR] FLOAT NOT NULL,
+[Loan_Term_Months] INT NOT NULL,
+[Existing_Debt_ZAR] FLOAT NOT NULL
+)
+INSERT INTO [credit_std].[dbo].[fact_credit](Transaction_ID, Annual_Income_ZAR, Monthly_Expenses_ZAR, 
+Credit_Score, Loan_Amount_ZAR, Loan_Term_Months, Existing_Debt_ZAR)
+SELECT DISTINCT  
+[Transaction_ID]
+,[Annual_Income_ZAR]
+,[Monthly_Expenses_ZAR]
+,[Credit_Score]
+,[Loan_Amount_ZAR]
+,[Loan_Term_Months]
+,[Existing_Debt_ZAR]
+FROM [credit_std].[dbo].[credit_risk_10k_records];
+
+SELECT *
+FROM [credit_std].[dbo].[fact_credit]
