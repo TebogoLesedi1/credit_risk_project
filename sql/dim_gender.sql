e9643d26d9e39686a9a38625f48d630db58088c2
TebogoLesedi1 <leseditebogo22@gmail.com>
2026-05-05 00:39:38 +0200

    feat: add and populate dim_gender table

---
+++ sql/dim_gender.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS [credit_std].[dbo].[dim_gender];
+CREATE TABLE [credit_std].[dbo].[dim_gender](
+	[user_ID] INT IDENTITY(1,1) PRIMARY KEY,
+	[Gender] [nvarchar](50) NOT NULL,
+	
+)
+INSERT INTO [credit_std].[dbo].[dim_gender](Gender)
+ SELECT DISTINCT
+[Gender]
+FROM [credit_std].[dbo].[credit_risk_10k_records];
+
+SELECT *
+FROM [credit_std].[dbo].[dim_gender]
+
