6eb16d6361203ae9bdedfd2bbf66dd451568442f
TebogoLesedi1 <leseditebogo22@gmail.com>
2026-05-05 00:27:15 +0200

    security: apply hard masking to sensitive user identifiers during ETL process

---
+++ sql/dim_user.sql
@@ -0,0 +1,21 @@
+----Dim_User--------------
+DROP TABLE IF EXISTS [credit_std].[dbo].[dim_user];
+CREATE TABLE [credit_std].[dbo].[dim_user](
+	[user_ID] INT IDENTITY(1,1) PRIMARY KEY,
+	[SA_ID_Number] [nvarchar](50) NOT NULL,
+	[First_Name] [nvarchar](50) NOT NULL,
+	[Last_Name] [nvarchar](50) NOT NULL,
+	[DOB] DATE NOT NULL
+)
+INSERT INTO [credit_std].[dbo].[dim_user](SA_ID_Number, First_Name, Last_Name, DOB)
+ SELECT DISTINCT
+ LEFT([SA_ID_Number],6) + '*******' + RIGHT([SA_ID_Number],3) AS MASKED_SA_ID
+,[First_Name]
+,[Last_Name]
+,[DOB]
+FROM [credit_std].[dbo].[credit_risk_10k_records];
+
+SELECT *
+FROM [credit_std].[dbo].[dim_user]
+
+
